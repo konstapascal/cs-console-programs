705d08d3af74f1c9d02f3f3ff7a5e6eff371ac6d
konstapascal <pascalkonstantinos@gmail.com>
2021-10-19 05:28:31 +0200

    implemented initial simple calculator, + and - support

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace csharp_console_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter operand (+ or -):");
+            string operand = Console.ReadLine();
+
+            Console.WriteLine("\nEnter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nNumber 1 was " + num1);
+            Console.WriteLine("Number 2 was " + num2);
+            Console.WriteLine("Operand was " + operand);
+
+            switch (operand)
+            {
+                case "+":
+                    Console.WriteLine("\nSum is: " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                default:
+                    Console.WriteLine("Provided operand is not valid.");
+                    break;
+            }
         }
     }
 }
